000d8e023caa61e0b81ca8d96cc119af86731202
Ethan Villasin <duke129409@gmail.com>
2025-06-27 09:17:31 -0400

    finished the last feature, nagivating to the directory where the go pro splits are

--- GoProSplicer.py
+++ GoProSplicer.py
@@ -3,12 +3,31 @@
 
 import os, re, subprocess
 
+isWSL = input("Are we in WSL? ").lower() in ('yes', 'y')
+
 isInDirectory = input("Are we already in the directory you want? ").lower() in ('yes', 'y')
+
+def folderSearch(folderName, drive):
+    command = [
+        "powershell.exe",
+        "-Command",
+        f"Get-ChildItem -Path {drive}:\\ -Recurse -Directory "
+        f"| Where-Object {{$_.Name -like '*{folderName}*'}} "
+        f"| Select-Object -First 1 -ExpandProperty FullName"
+    ]
+    result = subprocess.run(command, capture_output=True, text=True)
+    path = result.stdout.strip()
+    return path if path else None
+
 if not isInDirectory:
     drive = input("What drive will we be working in? ")
-    os.chdir("/mnt/" + drive.lower())
+    if isWSL:
+        os.chdir("/mnt/" + drive.lower())
+    else:
+        os.chdir(drive.lower())
     directory = input("Input directory of Go Pro Spilts: ")
-    #os.chdir
+    #print(folderSearch(directory, drive))
+    os.chdir("/mnt/" + folderSearch(directory, drive).replace("\\", "/").lower().replace(":",""))
 #print(os.getcwd())
 #Get all the files into a list
 allFiles = os.listdir()
@@ -52,6 +71,7 @@ for i, g in enumerate(grouped, start=1):
     with open(textFileName + '.txt', 'w') as f:
         for name in g:
             f.write('file ' + name + '\n')
+            print(textFileName + " created")
             
 #run ffmpeg command: ffmpeg -f concat -safe 0 -i filelist.txt -c copy output.mp4
 for i in GoProTextFiles:
